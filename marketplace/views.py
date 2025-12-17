2d11a2e79c01c3ebbbbef9c2dbe8d2986d521fba
Jamil Juma <jumajamil314@gmail.com>
2025-12-18 00:53:31 +0300

    Update .env and finalize backend homepage template

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("<h1>Welcome to the Marketplace Homepage!</h1>")
+    return render(request, "api_home.html")
